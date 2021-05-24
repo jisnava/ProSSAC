e1347b0b6496d9a3efb31358f42744cb465d0407
jisnava <jisnava@gmail.com>
2021-05-25 00:13:55 +0530

    Update 9_difference_calculator.py

--- DatasetConstruction/9_difference_calculator.py
+++ DatasetConstruction/9_difference_calculator.py
@@ -1,2 +1,2 @@
-#command to get difference between two text files of same type
+#command to get difference between two text files of same type, Run in terminal only
 comm -23 <(sort Pisces_40ID_A_CHAIN_ID_only.txt) <(sort 13_extracted_ID_check_list.txt) > 18_difference_A_chain_IDs.txt
