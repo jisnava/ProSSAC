476f07e542d32851f0ab226f753f8b713bb72cda
jisnava <jisnava@gmail.com>
2021-05-24 23:41:11 +0530

    Update 7_a_ids_not_in_secondary_file.py

--- DatasetConstruction/7_a_ids_not_in_secondary_file.py
+++ DatasetConstruction/7_a_ids_not_in_secondary_file.py
@@ -1,6 +1,4 @@
-#Program to extract primary and secondary data from ss.txt(secondary structure information from PDB) of a given protein Ids 
-#Of the proteins we have choosen as non redundant, we have those protein id's with A chain only
-#Here we will check if any of our protein details are not there in ss.txt(6_A_CHAIN_SECSTR.txt-A chain details from ss.txt)
+#Prints those PDB ID's whose secondary details are not present in 6_A_CHAIN_SECSTR.txt( A chain details extracted fron ss.txt)
 
 with open('6_A_CHAIN_SECSTR.txt','r') as f:
     haystack = f.read()
@@ -15,30 +13,6 @@ with open('1_Pisces_40ID_A_CHAIN_ID_only.txt','r') as f1:
 	for needle in (line.strip() for line in f1):
 		if needle not in haystack:
 			print(needle)
-
-
-		"""
-	#for line in f1:
-		#needle = line.strip()
-		print(needle)
-		#print("\n")
-		for line1 in haystack.splitlines():
-			if needle in line1:
-				#print(i,'found')
-				i=i+1
-				always_print= True
-				#print(line1, end='')
-				print(line1)
-				#print("\n")
-				#f2.write(line1)
-			elif '>' not in line1 and always_print:
-				print(line1)
-				#print(i)
-				j=j+1
-				#f2.write(line1)
-			elif '>' in line1:
-				always_print = False
-	"""		
 
 
 
