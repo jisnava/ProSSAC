fc16d1e091911fe01e7a9685045ee119cec90879
jisnava <jisnava@gmail.com>
2021-05-24 23:30:59 +0530

    Update 4_CA_chainwise.py

--- DatasetConstruction/4_CA_chainwise.py
+++ DatasetConstruction/4_CA_chainwise.py
@@ -1,5 +1,5 @@
 #Extracts only CA ATOM line from 'pdb' file
-#Do extract details of only first chain(first chain)
+#Do extract details of only first chain(any chain name)
 import os
 import math
 import glob
@@ -20,25 +20,23 @@ for file in os.listdir(directory_path1):
 		#print(lines[0])
 		#aa_name=currentline[3]
 		chain_name_old=lines[0][21:22]
-		#chain_name_old=str(chain_name_old)[0]
-		#chain_name_old=str(chain_name_old)
+		
 		res_num_old=lines[0][22:26]
 	except:
 		print("do nothing")
 	newfile1 = open('/home/jisna/Jisna/18_FULL_LENGTH_DATA_FOR_ASSIGNMENT/18_PDB_CA_A/' + file + "".join("_") +  "".join(chain_name_old), "a+")
-	#newfile2 = open('/home/jisna/Desktop/Akhil_Vinod_2019/DATASET_1442/PDB_CA/' + file + 'chain_name', "a+")
+	
 	for j in (lines):
-		#print(j)
-		#currentline=j.split()
+		
 		try:
-			#aa_name=currentline[3]
+			
 			chain_name=j[21:22]
 			#print(chain_name)
 			res_num=j[22:26]
 			if ( (chain_name==chain_name_old) and (res_num >= res_num_old)):
 				#L.append(j)
 				#print(j)
-				#if (str(j[16:17])=='B' or str(j[16:17])=='C'):
+				#if (str(j[16:17])=='B' or str(j[16:17])=='C'): However user can avoid specific chain names if needed
 					#continue
 				#else:
 				newfile1.writelines(''.join(j[17:20]+' '+j[30:38]+' '+j[38:46]+' '+j[46:54]+'\n'))
