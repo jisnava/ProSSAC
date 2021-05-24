55fcf4df579dba11b61dd188af8e0e9d119ec802
jisnava <jisnava@gmail.com>
2021-05-24 23:29:13 +0530

    Update 3_CA_extraction.py

--- DatasetConstruction/3_CA_extraction.py
+++ DatasetConstruction/3_CA_extraction.py
@@ -1,4 +1,4 @@
-#Extracts only CA ATOM RECORD line from 'pdb' file
+#Extracts only CA ATOM RECORD line from the protein 'pdb' files
 import os
 import math
 import glob
@@ -17,10 +17,7 @@ for file in os.listdir(directory_path1):
 	for j in (lines):
 		#currentline=j.split()
 		try:
-			#aa_name=currentline[3]
-			#atom_name=currentline[2]
-			#print(j[0:4])
-			#print(j[13:15])
+			
 			if ((str(j[0:4])=='ATOM') and (str(j[13:15]) =='CA')):
 				L.append(j)
 				#print(j)
